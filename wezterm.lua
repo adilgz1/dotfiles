1285d482738ce49ca5c86ef6cd5b0287ed46f8c3
adilgz1 <adilghaznavi16@gmail.com>
2026-07-23 05:23:51 +0500

    wezterm opacity

--- wezterm.lua
+++ wezterm.lua
@@ -24,7 +24,7 @@ config.font_size = 13
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.5
+config.window_background_opacity = 0.6
 config.macos_window_background_blur = 30
 -- Enable window borders and integrated control buttons
 
