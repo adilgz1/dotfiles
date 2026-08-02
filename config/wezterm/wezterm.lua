0ffc32568c6ae4ef66ded7061f80d47ff0c3800e
Adil <adilghaznavi16@gmail.com>
2026-08-02 19:29:19 +0500

    changed window padding

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -33,5 +33,13 @@ config.window_background_opacity = 0.6
 config.macos_window_background_blur = 5
 -- Enable window borders and integrated control buttons
 
+-- window padding adjust
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 -- and finally, return the configuration to wezterm
 return config
