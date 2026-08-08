47213c722f91951495dae8f37e53864b73a4e092
Adil <adilghaznavi16@gmail.com>
2026-08-08 16:53:49 +0500

    fixed eza icons issue and zsh history fzf

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -1,9 +1,7 @@
 # =========================================================
 # Plugins
 # =========================================================
-
 ZPLUGINDIR="${ZDOTDIR:-$HOME/.config/zsh}/plugins"
-
 _zplugin_load() {
   local plugin_path="${ZPLUGINDIR}/${2}"
   if [[ ! -d "$plugin_path" ]]; then
@@ -14,13 +12,25 @@ _zplugin_load() {
   fi
   source "${plugin_path}/${2}.plugin.zsh"
 }
-
 zplugin-update() {
   local dir
   for dir in "${ZPLUGINDIR}"/*/; do
     echo "Updating ${dir:t}..."
     git -C "$dir" pull --ff-only
   done
+}
+
+# Re-apply fzf's Ctrl-R binding after zsh-vi-mode sets up its keymaps
+function zvm_after_init() {
+  if [[ -f /opt/homebrew/opt/fzf/shell/key-bindings.zsh ]]; then
+    source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
+  elif [[ -f /usr/local/opt/fzf/shell/key-bindings.zsh ]]; then
+    source /usr/local/opt/fzf/shell/key-bindings.zsh
+  elif [[ -f /usr/share/fzf/key-bindings.zsh ]]; then
+    source /usr/share/fzf/key-bindings.zsh
+  elif [[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]]; then
+    source /usr/share/doc/fzf/examples/key-bindings.zsh
+  fi
 }
 
 _zplugin_load zsh-users zsh-autosuggestions
