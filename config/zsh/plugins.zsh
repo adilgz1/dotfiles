703143ff14e1460b3e5700c0b02c1f7d2e89d905
adilgz1 <adilghaznavi16@gmail.com>
2026-08-10 18:52:21 +0500

    changing zsh plugin load order again for toolbox fuckery

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -33,7 +33,7 @@ function zvm_after_init() {
   fi
 }
 
+_zplugin_load zdharma-continuum fast-syntax-highlighting
 _zplugin_load zsh-users zsh-autosuggestions
 _zplugin_load zsh-users zsh-history-substring-search
-_zplugin_load zdharma-continuum fast-syntax-highlighting
 _zplugin_load jeffreytse zsh-vi-mode
