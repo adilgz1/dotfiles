d9cb8c9c0686ee1764e6367628267cefc2e2fa00
Adil <adilghaznavi16@gmail.com>
2026-09-09 21:52:59 +0500

    vimtex shit

--- config/nvim/lua/adil/plugins/vimtex.lua
+++ config/nvim/lua/adil/plugins/vimtex.lua
@@ -4,5 +4,16 @@ return {
   config = function()
     vim.g.vimtex_view_method = os.getenv("VIMTEX_VIEWER") or "zathura"
     vim.g.vimtex_compiler_method = "latexmk"
+
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = "build",
+      out_dir = ".",
+      options = {
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+      },
+    }
   end,
 }
