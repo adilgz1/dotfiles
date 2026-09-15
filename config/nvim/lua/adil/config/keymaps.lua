5d7a54201d675ddfbb4e3648907f7373bd582add
Adil <adilghaznavi16@gmail.com>
2026-09-15 13:52:39 +0500

    change <leader>qq to quit all buffers

--- config/nvim/lua/adil/config/keymaps.lua
+++ config/nvim/lua/adil/config/keymaps.lua
@@ -54,7 +54,7 @@ keymap.set("n", "<leader>ww", ":w<CR>", { desc = "save" })
 keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "save and quit" })
 
 -- force quit
-keymap.set("n", "<leader>qq", ":q!<CR>", { desc = "force quit" })
+keymap.set("n", "<leader>qq", ":qa!<CR>", { desc = "force quit" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
